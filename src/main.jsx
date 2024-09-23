99856009ff8d6a4ebc04942f89368faab84405b9
GankiDev <giancarlonsito@gmail.com>
2024-09-23 11:02:11 -0400

    Adding vercel analitycddfst

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from "./App.jsx";
 import "./main.css";
 
 createRoot(document.getElementById("root")).render(
-    <Analytics>
-        <App />ss
-    </Analytics>
+        <App>
+            <Analytics/>
+        </App>
 );
